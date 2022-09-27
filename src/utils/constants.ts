afb43ade1e42861d4c25ef3d1a407a400db64904
Jefferson00 <jeffersonts00@gmail.com>
2022-09-27 11:24:20 -0300

    add config env vars

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
-export const PACKAGE_TIMOUT = 5;
+export const PACKAGE_TIMOUT =
+  Number(process.env.NEXT_PUBLIC_PACKAGE_TIMOUT) || 5;
 export const POKEMON_QTD = 898;
-export const SHINY_PROBABILITY = 40;
+export const SHINY_PROBABILITY =
+  Number(process.env.NEXT_PUBLIC_SHINY_PROBABILITY) || 40;
 export const CARD_QTD_PER_PACKAGE = 5;
 export const MIN_QTD_TO_TRADE = 5;
